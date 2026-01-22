986be5f39f0a966ee5513fa7df3074161ea8f3b5
alexUkkonen <alex.ukkonen.03@gmail.com>
2026-01-22 12:32:21 +0200

    first commit

---
+++ src/main/java/fi/haagahelia/bookstore/storage/Book.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.bookstore.storage;
+
+import java.math.BigDecimal;
+
+public class Book {
+    private String title;
+    private String author;
+    private int publicationYear;
+    private String isbn;
+    private BigDecimal price;
+
+
+}
