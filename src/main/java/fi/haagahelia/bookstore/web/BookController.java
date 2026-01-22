2ce666f188b66afedd54d2f872a8cabd7246944b
alexUkkonen <alex.ukkonen.03@gmail.com>
2026-01-22 12:33:27 +0200

    made index page

--- src/main/java/fi/haagahelia/bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -1,5 +1,12 @@
 package fi.haagahelia.bookstore.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class BookController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
 
 }
