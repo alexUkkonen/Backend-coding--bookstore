3037f604affb58b3952c8cc2e6c827f73eac5351
Home_PC <alex.ukkonen.03@gmail.com>
2026-01-29 12:35:57 +0200

    add addBook view and saveBook functionality in BookController

--- src/main/java/fi/haagahelia/bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 
 import fi.haagahelia.bookstore.storage.BookRepository;
+import fi.haagahelia.bookstore.storage.Book;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @Controller
 public class BookController {
@@ -20,5 +25,19 @@ public class BookController {
         model.addAttribute("books", bookRepository.findAll());
         return "bookList";
     }
+
+    @GetMapping("/addbook") // Simple get mapping to add a new book
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/savebook") // Post mapping to save added book
+    public String saveBook(Book book) {
+        bookRepository.save(book);
+        return "redirect:/booklist";
+    }
+    
+    
 
 }
