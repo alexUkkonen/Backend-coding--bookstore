5f18ec3a5f5c3465401542a792d7d92ee83e1036
alexUkkonen <alex.ukkonen.03@gmail.com>
2026-01-29 08:39:05 +0200

    refactor Book class constructor for clarity

--- src/main/java/fi/haagahelia/bookstore/storage/Book.java
+++ src/main/java/fi/haagahelia/bookstore/storage/Book.java
@@ -9,5 +9,11 @@ public class Book {
     private String isbn;
     private BigDecimal price;
 
-
+    public Book(String title, String author, int publicationYear, String isbn, BigDecimal price) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.isbn = isbn;
+        this.price = price;
+    }
 }
